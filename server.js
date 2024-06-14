fc5f4987179c6673a2071737419c8238e7ffb409
Alison Rocha <rocha7andrade@gmail.com>
2024-06-14 14:14:43 -0300

    configurando socket.io

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/socket.io', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://p2p-crypto-app.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  next();
+});
+
 
 let users = [];
 let transactions = [];
